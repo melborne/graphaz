b85216bcc2e64a6cefcb75eba3305222965b9b7f
melborne <postagie@gmail.com>
2010-02-18 07:06:14 +0900

    FIX:for < ruby190 ADD:nodes accessor

--- graphaz.rb
+++ graphaz.rb
@@ -2,13 +2,12 @@ require "graphviz"
 require "RMagick"
 
 class GraphAz
-  attr_accessor :graph, :edges, :gnode, :gedge
+  attr_accessor :graph, :nodes, :edges, :gnode, :gedge
   def initialize(name= :G, *opt, &blk)
     @graph = GraphViz.new(name, *opt, &blk)
     @gnode, @gedge = @graph.node, @graph.edge
-    @edges = {}
-    @@lap = 0
-    @laps = []
+    @nodes, @edges = [], {}
+    @@lap, @laps = 0, []
   end
 
   def [](attr)
@@ -21,20 +20,20 @@ class GraphAz
   
   def add(s, attrs={})
     names = s.split(/\s*=>\s*/).map { |n| n.sub(/^:/, '') }
-    names.map! { |name| @graph.add_node(name, attrs) }
+    names.map! { |name| @nodes << node = @graph.add_node(name, attrs); node }
     names.each_cons(2) do |a, b|
       @edges["#{a.name}_#{b.name}"] = @graph.add_edge(a, b)
     end
     @graph
   end
   
-  def node(*nodes, opts)
+  def node(nodes, opts)
     nodes.each do |node|
       opts.each { |attr, val| @graph.get_node(node)[attr] = val  }
     end
   end
   
-  def edge(*edges, opts)
+  def edge(edges, opts)
     @edges.each do |name, edge|
       next unless edges.empty? or edges.include?(name)
       opts.each { |attr, val| edge.set { |e| e.send(attr, val) } }
@@ -88,7 +87,7 @@ if __FILE__ == $0
   }
 
   routes.each { |route| ga.add route }
-  edge_labels.each { |label, edges| ga.edge(*edges, :label => label) }
+  edge_labels.each { |label, edges| ga.edge(edges, :label => label) }
   ga.lap
 
   color = 'blueviolet'
