49f9e5f879bf6e5e7e03b7cc4c659942d2518e9a
kyoendo <postagie@gmail.com>
2010-10-25 13:23:43 +0900

    add group method for creating clusters

--- graphaz.rb
+++ graphaz.rb
@@ -1,5 +1,4 @@
 require "graphviz"
-require "RMagick"
 
 class GraphAz
   attr_accessor :graph, :nodes, :edges, :gnode, :gedge
@@ -8,6 +7,7 @@ class GraphAz
     @gnode, @gedge = @graph.node, @graph.edge
     @nodes, @edges = [], {}
     @@lap, @laps = 0, []
+    @groups = []
   end
 
   def [](attr)
@@ -19,12 +19,24 @@ class GraphAz
   end
   
   def add(s, attrs={})
+    if group = attrs.delete(:group)
+      add_group(group)
+    end
     names = s.split(/\s*=>\s*/).map { |n| n.sub(/^:/, '') }
+    #add_node
     nodes = names.inject([]) do |mem, name|
       attrs[:label] = name
-      @nodes << node = @graph.add_node(name, attrs)
+      receiver = @graph
+      if group
+        @graph.send("cluster_#{@groups.index(group)}") do |c|
+          c[:label => group]
+          receiver = c
+        end
+      end
+      @nodes << node = receiver.add_node(name, attrs)
       mem << node
     end
+    #add_edge
     nodes.each_cons(2) do |a, b|
       @edges["#{a.id}_#{b.id}"] = @graph.add_edge(a, b)
     end
@@ -44,6 +56,25 @@ class GraphAz
     end
   end
 
+  #WARN: need to call before add method
+  def group(gname, parent=nil)
+    add_group(gname)
+    receiver = @graph
+    if parent #nested group
+      add_group(parent)
+      @graph.send("cluster_#{@groups.index(parent)}") do |c|
+        c[:label => parent]
+        receiver = c
+      end
+    end
+    receiver.send("cluster_#{@groups.index(gname)}") { |cl| cl[:label => gname] }
+  end
+
+  def add_group(name)
+    @groups.none? { |g| g == name } ? @groups << name : nil
+  end
+  private :add_group
+
   def print_graph(args={}, &blk)
     @graph.output(args, &blk)
   end
@@ -55,6 +86,7 @@ class GraphAz
   end
   
   def write(opts={})
+    require "RMagick"
     puts 'processing...'
     opts = {:delay => 50, :filename => 'out.gif', :delete => true}.merge(opts)
     list = Magick::ImageList.new(*@laps)
--- tc_graphaz.rb
+++ tc_graphaz.rb
@@ -1,7 +1,7 @@
 require "test/unit"
-
 require_relative "graphaz"
 
+@@result = nil
 class TestGraphaz < Test::Unit::TestCase
   def setup
     @ga = GraphAz.new
@@ -15,4 +15,17 @@ class TestGraphaz < Test::Unit::TestCase
     assert_equal(expects, actuals)
   end
 
+  def test_cluster
+    @ga.group("lang", "world")
+    @ga.group("nolang", "world")
+    @ga.add(":ruby => :is => :programming => :language", :group => "lang")
+    @ga.add(":pearl => :isnot => :language", :group => "nolang")
+    @ga.add(":I => :like => :lisp", :group => "lang")
+    @ga.add(":java => javascript", :group => "world")
+    @ga.print_graph(:dot => 'out.dot')
+  end
 end
+
+END{END{
+  p @@result
+}}
